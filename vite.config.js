eac5121836d89500983c0b73851333fb6c2b7308
tomialvarez78@gmail.com <tomialvarez78@gmail.com>
2025-11-27 21:03:05 -0300

    General update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',        // ← ESTA LÍNEA SOLUCIONA LOS ESTILOS EN EL HOSTING
   plugins: [react()],
   server: {
     proxy: {
